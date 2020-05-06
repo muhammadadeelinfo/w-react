a053e37fccb44cf9e07828ceefb89a69ad791937
muhammadadeelinfo <muhammad.adeel.info@gmail.com>
2020-05-06 13:17:21 +0200

    added day and date

--- src/App.js
+++ src/App.js
@@ -5,11 +5,47 @@ const api = {
 };
 
 function app() {
+  const dateBuilder = (d) => {
+    let months = [
+      "January",
+      "February",
+      "March",
+      "April",
+      "May",
+      "June",
+      "July",
+      "August",
+      "September",
+      "October",
+      "November",
+      "December",
+    ];
+    let days = [
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday",
+      "Sunday",
+    ];
+
+    let day = days[d.getDay()];
+    let date = d.getDate();
+    let month = months[d.getMonth()];
+    let year = d.getFullYear();
+
+    return day + " " + date + " " + month + " " + year;
+  };
   return (
     <div className="app">
       <main>
         <div className="search-box">
           <input type="text" className="search-bar" placeholder="search..." />
+        </div>
+        <div className="location-box">
+          <div className="location">Berlin, DE</div>
+          <div className="data">{dateBuilder(new Date())}</div>
         </div>
       </main>
     </div>
