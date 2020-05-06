9cbcfafbfa18d855081f8dafd36d5002730a0252
muhammadadeelinfo <muhammad.adeel.info@gmail.com>
2020-05-06 11:55:03 +0200

    added background search box and introduced rest api

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import React from "react";
+const api = {
+  key: "caf477dce0feaf3caad36d0c31fdf3fd",
+  base: " https://api.openweathermap.org/data/2.5/",
+};
 
-function App() {
-  return <div className="App"></div>;
+function app() {
+  return (
+    <div className="app">
+      <main>
+        <div className="search-box">
+          <input type="text" className="search-bar" placeholder="search..." />
+        </div>
+      </main>
+    </div>
+  );
 }
 
-export default App;
+export default app;
